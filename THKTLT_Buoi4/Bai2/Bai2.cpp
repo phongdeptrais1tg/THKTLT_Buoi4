b6778cc72c3ebc09c8fc19d14fa59b05d9437940
phongdeptrais1tg <hoanganhphong2002hcm@gmail.com>
2024-08-01 18:46:16 +0700

    Buoi4_Bai2

---
+++ THKTLT_Buoi4/Bai2/Bai2.cpp
@@ -0,0 +1,37 @@
+﻿#include <stdio.h>
+
+
+int gcd(int a, int b) {
+    while (b != 0) {
+        if (a > b) {
+            a = a - b;
+        }
+        else {
+            b = b - a;
+        }
+    }
+    return a;
+}
+
+
+void menu() {
+    int a, b;
+
+    printf("Nhap hai so nguyen duong a va b:\n");
+    printf("a: ");
+    scanf("%d", &a);
+    printf("b: ");
+    scanf("%d", &b);
+
+    if (a <= 0 || b <= 0) {
+        printf("Hai so phai la cac so nguyen duong.\n");
+        return;
+    }
+
+    printf("UCLN(%d, %d) la: %d\n", a, b, gcd(a, b));
+}
+
+int main() {
+    menu();
+    return 0;
+}
